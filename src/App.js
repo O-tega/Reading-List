ed79e6211108e21504ef179872e960fa97950031
O-tega <tega.myview@gmail.com>
2022-09-22 15:00:50 +0100

    Create and consume context 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {
+	Component,
+} from "react";
+import Booklist from "./components/Booklist";
+import Navbar from "./components/Navbar";
+import ThemeContextProvider from "./contexts/ThemeContext";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	state = {};
+
+	render() {
+		return (
+			<div className='flex flex-col h-screen items-center'>
+				<div className='w-3/4 bg-white h-screen px-10'>
+					<ThemeContextProvider>
+						<Navbar />
+						<Booklist />
+					</ThemeContextProvider>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
