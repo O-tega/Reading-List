39ce683c168ba93dccc8ff59fe37f72534d4533c
O-tega <tega.myview@gmail.com>
2022-09-22 11:56:22 +0100

    Setting up react and tailwind configuration

---
+++ src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react";
+
+
+class Navbar extends Component{
+
+    state={}
+
+    render(){
+        return(
+            <div>
+                <nav>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>FAQ</li>
+                </nav>
+            </div>
+        )
+    }
+
+
+}
+
+
+export default Navbar;
