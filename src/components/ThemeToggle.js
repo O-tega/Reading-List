5b3a38bfb65c93f2f6a5f722f7dbeaf14802885f
O-tega <tega.myview@gmail.com>
2022-09-23 13:50:20 +0100

    install react-router-dom to be able to switch to hooks page

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,15 +1,24 @@
 import React, { Component } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import {Link} from 'react-router-dom'
 
 class ThemeToggle extends Component {
     static contextType = ThemeContext
 	state = {};
 
-    render() {
+render() {
         const {toogleTheme} = this.context
         return (
-            <div className="flex justify-center py-10 ">
-                <button onClick={toogleTheme} className=" rounded-lg shadow-lg bg-slate-400 text-white font-medium px-4 py-1">Toogle</button>
+            <div className="flex justify-center"> 
+                <div className="flex justify-center py-10 mr-5 ">
+                    <button onClick={toogleTheme} className=" rounded-lg shadow-lg bg-slate-400 text-white font-medium px-4 py-1">Toogle</button>
+                </div>
+
+               <div className="flex justify-center py-10 ">
+                    <Link to='/hooks-page'>
+                        <button className=" rounded-lg shadow-lg bg-slate-400 text-white font-medium px-4 py-1">React Hooks</button>
+                    </Link>
+                </div>
             </div>
             
             )
