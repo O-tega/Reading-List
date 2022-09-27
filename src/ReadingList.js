f840db7520370dbcd041926065ba5ad88134532f
O-tega <tega.myview@gmail.com>
2022-09-27 15:24:40 +0100

    created book list and book detail component

--- src/ReadingList.js
+++ src/ReadingList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import BookListRl from './components/readingListComponent/BookListRl';
 import Navbar from './components/readingListComponent/Navbar';
 import BookContextProvider from './contexts/BookContextRl';
 
@@ -8,6 +9,7 @@ const ReadingList = ()=>{
 			<div className=' h-screen rounded-t-3xl bg-[#4c2a4c]'>
 				<BookContextProvider>
 					<Navbar />
+                    <BookListRl/>
 				</BookContextProvider>
 			</div>
 		);
---
+++ src/components/readingListComponent/BookDetails.js
@@ -0,0 +1,23 @@
+import React, {useContext} from 'react';
+import { BookContext } from '../../contexts/BookContextRl';
+
+
+const BookDetail =({title, author, id})=>{
+
+    const{removeBook} = useContext(BookContext)
+
+    return(
+        <div onClick={()=>removeBook(id)}
+            className=' text-center text-white text-sm rounded bg-[#6d3d6d] mb-5 p-3 hover:line-through cursor-pointer hover:opacity-80'>
+            <p className="flex justify-between">
+                <span className="text-xs">{id}.</span>
+                <span className="font-medium">{title}</span>
+                <span>-</span>
+                <span className="text-xs">{author}</span>
+            </p>
+		</div>
+
+    )
+}
+
+export default BookDetail
