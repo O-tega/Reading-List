3bc73b9ba60a528c3b99ab5ed1789876e6004b4b
O-tega <tega.myview@gmail.com>
2022-09-27 12:39:56 +0100

    created bookContext for reading list

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import React, {
-	Component,
-} from "react";
-import {Routes, Route} from 'react-router-dom'
-import ContextHooks from "./components/ContextHooks";
+import React, {Component } from "react";
+import AppRoutes from "./AppRoutes";
+
 import Home from "./components/Home";
-import Context from "./Context"
-import Hooks from "./Hooks";
-import UseContext from './UseContext'
 
 class App extends Component {
-
 	render() {
 		return (
 			<div className='flex flex-col h-screen items-center'>
-      <Home/>
+				<Home />
 				<div className='w-3/4 bg-white h-screen rounded-t-3xl '>
-          <Routes>
-            <Route path='context&hooks' element={<ContextHooks/>}>
-              <Route path='context' element={<Context/>} />
-              <Route path='hooks' element={<Hooks/>} />
-              <Route path='usecontext' element={<UseContext/>} />
-            </Route>
-          </Routes>
-          </div>
+					<AppRoutes />
 				</div>
+			</div>
 		);
 	}
 }
---
+++ src/ReadingList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import BookContextProvider from './contexts/BookContextRl';
+
+
+const ReadingList = ()=>{
+    return (
+        <div>
+            <BookContextProvider>
+            
+            </BookContextProvider>
+            new reading list
+        </div>
+    )
+}
+
+
+export default ReadingList;
