673d3863727fd6b564f4ac1726cad57b83392197
O-tega <tega.myview@gmail.com>
2022-10-04 12:30:42 +0100

    modified booklist for mobile responsiveness

--- src/components/readingListComponent/BookListRl.js
+++ src/components/readingListComponent/BookListRl.js
@@ -5,12 +5,10 @@ import { BookContext } from "../../contexts/BookContextRl";
 import BookDetail from "./BookDetails";
 
 const BookListRl = () => {
-	const { books } = useContext(
-		BookContext
-	);
+	const { books } = useContext(BookContext);
 	return (
 		<div className=' flex justify-center '>
-			<div className='w-8/12 bg-[#8f548f] h-96 flex flex-col pt-8 px-10'>
+			<div className='w-10/12 md:w-8/12 bg-[#8f548f] h-[640px] md:h-96 flex flex-col pt-8 px-2 md:px-10'>
 				{books.length? books.map(
 					({ title, author, id }) => {
 						return (
@@ -20,9 +18,9 @@ const BookListRl = () => {
 						);
 					}
 				):
-            <div className="text-white text-center bg-[#6d3d6d] rounded-lg py-24 mx-20 ">
-                <p className="text-lg font-bold">No Books to read</p>
-                <p className="text-3xl font-bold">Hello free time 🕺💃</p>
+            <div className="text-white text-center bg-[#6d3d6d] rounded-lg mx-0 py-10 md:py-10 md:mx-10 ">
+                <p className="text-xs md:text-lg md:font-bold">No Books to read</p>
+                <p className="text-md md:text-3xl md:font-bold">Hello free time 🕺💃</p>
         </div> }
 			</div>
 
--- src/components/readingListComponent/Navbar.js
+++ src/components/readingListComponent/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = ()=>{
 
     return(
         <div className='flex justify-center pt-10'>
-            <div className='w-8/12 rounded-t-lg flex flex-col items-center py-5 bg-[#6d3d6d]'>
+            <div className='w-10/12 md:w-8/12 rounded-t-lg flex flex-col items-center py-5 bg-[#6d3d6d]'>
             <h1 className="font-bold text-2xl text-center text-white">My Reading List</h1>
             <p className='text-center text-white pt-2 text-sm'>Currently you have <span className='font-bold px-2  border-white border rounded-full'>{books.length}</span> books to get through...</p>
             </div>
